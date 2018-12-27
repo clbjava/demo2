42cb8078c9861e274a4a7073d65fd25173c8dff5
Administrator <295435732@qq.com>
2018-12-27 17:58:00 +0800

    Initial commit

---
+++ demo2-1/src/main/java/com/demo/Test2.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import com.demo.s.Demo4;
+
+/**
+ * @Title: com.demo
+ * @Description: TODO
+ * @author: Administrator
+ * @date: 2018-12-27 17:09
+ * @version: 1.0.0
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        Demo4 de=new Demo4();
+        de.say();
+
+    }
+
+}
---
+++ demo2-2/src/main/java/com/demo/s/Demo4.java
@@ -0,0 +1,17 @@
+package com.demo.s;
+
+/**
+ * @Title: com.demo
+ * @Description: TODO
+ * @author: Administrator
+ * @date: 2018-12-27 17:10
+ * @version: 1.0.0
+ */
+public class Demo4 {
+
+    public void say(){
+        System.out.printf("heloo world!");
+        System.out.printf("heloo world!");
+
+    }
+}
